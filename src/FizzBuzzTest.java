a9925579843ba25cdacfcd2211d72f00ad57b5fd
Tim Johnson <pavementii@gmail.com>
2018-01-24 17:11:23 -0500

    Refactored FizzBuzz

--- src/FizzBuzzTest.java
+++ src/FizzBuzzTest.java
@@ -1,26 +1,31 @@
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 import com.pillar.FizzBuzz;
 
 public class FizzBuzzTest {
+	FizzBuzz fizzBuzz;
+	
+	@Before
+	public void setUp() {
+		fizzBuzz = new FizzBuzz();
+	}
+	
 	@Test
 	public void whenFizzBuzzIsPassedANumberItReturnsThatNumber() {
-		FizzBuzz fizzBuzz = new FizzBuzz();
 		assertEquals("1", fizzBuzz.fizz(1));
 	}
 
 	@Test
 	public void whenFizzBuzzIsPassedATwoItReturnsATwo() {
-		FizzBuzz fizzBuzz = new FizzBuzz();
 		assertEquals("2", fizzBuzz.fizz(2));
 	}
 	
 	@Test
 	public void whenFizzBuzzIsPassedAMultipleOfThreeItReturnsFizz() {
-		FizzBuzz fizzBuzz = new FizzBuzz();
 		assertEquals("fizz", fizzBuzz.fizz(3));
 		assertEquals("fizz", fizzBuzz.fizz(6));
 	}
